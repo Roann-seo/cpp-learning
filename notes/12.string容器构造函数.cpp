0ef56e6204c728ee3051cac27677bd0490f9842f
Roann-seo <liangxin12366@outlook.com>
2026-07-23 22:49:26 +0800

    📝 完善排序算法 + string容器构造函数笔记

--- notes/12.string容器构造函数.cpp
+++ notes/12.string容器构造函数.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 #include<string>
 using namespace std;
+//string的本质是类不是指针，类内封装了char*,是char*型的容器
+//
 void test01(){
-    string s1;
+    string s1;//创建空字符串
     const char*str="hello world";
-    string s2(str);
+    string s2(str);//使用字符串str初始化
     cout<<s2<<endl;
-    string s3(s2);
+    string s3(s2);//使用一个string对象初始化另一个string对象
     cout<<s3<<endl;
-    string s4(10,'a');//aaaaaaaaaa
-    cout<<s4<<endl;
+    string s4(10,'a');//使用n个字符c初始化
+    cout<<s4<<endl;//aaaaaaaaaa
 
 }
 int main(){
--- notes/C++笔记/29.排序.cpp
+++ notes/C++笔记/29.排序.cpp
@@ -34,8 +34,23 @@ void test02(){
     for_each(v.begin(),v.end(),myprint);
     cout<<endl;
 }
+//merge合并两个有序序列
+void test03(){
+    vector<int>v1;
+    vector<int>v2;
+    for(int i=0;i<10;i++){
+        v1.push_back(i);
+        v2.push_back(i+1);
+    }
+    vector<int>v;
+    v.resize(v1.size()+v2.size());
+    merge(v1.begin(),v1.end(),v2.begin(),v2.end(),v.begin());
+    for_each(v.begin(),v.end(),myprint);
+    cout<<endl;
+}
 int main(){
     //test01();
-    test02();
+    // test02();
+    test03();
     return 0;
 }
